ec9777c896c3c4663a1a42e1de86121f27f9b12c
nicolas_jenichen <nicolas_jenichen@estudante.sesisenai.org.br>
2023-09-14 19:33:59 -0300

    atividade6

---
+++ atividade6.java
@@ -0,0 +1,10 @@
+public class atividade6 {
+
+     public static void main(String[] args) {
+ 
+        for (int i = 1 ; i <= 100 ; i=i+1){
+             
+             System.out.println(i % 10 == 0 ? i + " multiplo de 10": i ); 
+         }
+    }
+}
