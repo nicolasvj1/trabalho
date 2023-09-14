cb76d71b82aab725c2fe28fd44ab98e8f765fc3f
nicolas_jenichen <nicolas_jenichen@estudante.sesisenai.org.br>
2023-09-14 19:21:47 -0300

    atividade5

---
+++ atividade5.java
@@ -0,0 +1,13 @@
+public class atividade5 {
+   
+     public static void main(String[] args) {
+
+        double soma = 0;
+ 
+        for (int i = 2 ; i <= 100 ; i=i+2){
+             System.out.println(i);
+             soma = soma + i; 
+         }
+         System.out.println(soma);
+    }
+}
