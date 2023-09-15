f521fd2a70e15dd54576fc54f49682f1340434ce
nicolas_jenichen <nicolas_jenichen@estudante.sesisenai.org.br>
2023-09-14 21:49:38 -0300

    string

---
+++ string.java
@@ -0,0 +1,10 @@
+public class string {
+
+    public static void main(String[] args) {
+        String teste = "Aula do Calvo Lindão";
+
+        System.out.println("O com\nprime\nnto da s\rtrin\tg é: ");
+
+    }
+    
+}
